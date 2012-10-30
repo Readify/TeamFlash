742b645b3407509004b5369d0da02ed2cf74db82
Ben Phegan <pheganb@L10000053.aud01.cbaidev01.com>
2012-10-30 15:34:53 +1100

    Ugly try...catch, need to refactor this thing completely.

--- TeamFlash/Monitor.cs
+++ TeamFlash/Monitor.cs
@@ -16,62 +16,72 @@ namespace TeamFlash
 
         public void SetLed(byte led, bool turnItOn, bool flashIt, bool turnOffAfterFlashing = false , float? flashDurationInMilliSeconds = null, float? flashFrequency = null, Func<bool> finishWhen = null)
         {
-            var hUsb = GetDelcomDeviceHandle(); // open the device
-            DateTime? until = null;
-            if (finishWhen == null)
+            try
             {
-                finishWhen = () =>
-                    {
-                        if (!until.HasValue)
-                            until = DateTime.Now.AddSeconds(Convert.ToInt32(flashDurationInMilliSeconds / 1000));
-                        return DateTime.Now < until;
-                    };
-            }
-            if (hUsb == 0) return;
-            if (turnItOn)
-            {
-                if (flashIt)
-                {
-                    if (flashFrequency.HasValue)
-                    {
-                        var toggle = true;
-                        while (finishWhen())
-                        {
-                            
-                            DelcomBuildIndicator.DelcomLEDControl(hUsb, led, toggle ? DelcomBuildIndicator.LEDOFF : DelcomBuildIndicator.LEDON);
-                            Thread.Sleep(Convert.ToInt16(flashFrequency.Value));
-                            toggle = !toggle;
-                            switch (led)
-                            {
-                                case DelcomBuildIndicator.GREENLED :
-                                    led = DelcomBuildIndicator.BLUELED;
-                                    break;
-                                case DelcomBuildIndicator.BLUELED :
-                                    led = DelcomBuildIndicator.REDLED;
-                                    break;
-                                case DelcomBuildIndicator.REDLED :
-                                    led = DelcomBuildIndicator.GREENLED;
-                                    break;
-                            }
-                        }
-                        var ledStatus = turnOffAfterFlashing ? DelcomBuildIndicator.LEDOFF : DelcomBuildIndicator.LEDON;
 
+                var hUsb = GetDelcomDeviceHandle(); // open the device
+                DateTime? until = null;
+                if (finishWhen == null)
+                {
+                    finishWhen = () =>
+                        {
+                            if (!until.HasValue)
+                                until = DateTime.Now.AddSeconds(Convert.ToInt32(flashDurationInMilliSeconds/1000));
+                            return DateTime.Now < until;
+                        };
+                }
+                if (hUsb == 0) return;
+                if (turnItOn)
+                {
+                    if (flashIt)
+                    {
+                        if (flashFrequency.HasValue)
+                        {
+                            var toggle = true;
+                            while (finishWhen())
+                            {
+
+                                DelcomBuildIndicator.DelcomLEDControl(hUsb, led,
+                                                                      toggle
+                                                                          ? DelcomBuildIndicator.LEDOFF
+                                                                          : DelcomBuildIndicator.LEDON);
+                                Thread.Sleep(Convert.ToInt16(flashFrequency.Value));
+                                toggle = !toggle;
+                                switch (led)
+                                {
+                                    case DelcomBuildIndicator.GREENLED:
+                                        led = DelcomBuildIndicator.BLUELED;
+                                        break;
+                                    case DelcomBuildIndicator.BLUELED:
+                                        led = DelcomBuildIndicator.REDLED;
+                                        break;
+                                    case DelcomBuildIndicator.REDLED:
+                                        led = DelcomBuildIndicator.GREENLED;
+                                        break;
+                                }
+                            }
+                            var ledStatus = turnOffAfterFlashing
+                                                ? DelcomBuildIndicator.LEDOFF
+                                                : DelcomBuildIndicator.LEDON;
+
+                        }
+                        else
+                        {
+                            DelcomBuildIndicator.DelcomLEDControl(hUsb, led, DelcomBuildIndicator.LEDFLASH);
+                        }
                     }
                     else
                     {
-                        DelcomBuildIndicator.DelcomLEDControl(hUsb, led, DelcomBuildIndicator.LEDFLASH);
+                        DelcomBuildIndicator.DelcomLEDControl(hUsb, led, DelcomBuildIndicator.LEDON);
                     }
                 }
                 else
                 {
-                    DelcomBuildIndicator.DelcomLEDControl(hUsb, led, DelcomBuildIndicator.LEDON);
+                    DelcomBuildIndicator.DelcomLEDControl(hUsb, led, DelcomBuildIndicator.LEDOFF);
                 }
+                DelcomBuildIndicator.DelcomCloseDevice(hUsb);
             }
-            else
-            {
-                DelcomBuildIndicator.DelcomLEDControl(hUsb, led, DelcomBuildIndicator.LEDOFF);
-            }
-            DelcomBuildIndicator.DelcomCloseDevice(hUsb);
+            catch(Exception){}
         }
 
         readonly StringBuilder deviceName = new StringBuilder(DelcomBuildIndicator.MAXDEVICENAMELEN);
