cb5e39aa282924768519da3b8516e11680cee72b
Ben Phegan <benphegan@gmail.com>
2013-09-25 11:11:46 +1000

    Add subnet scan capability.

--- TeamFlash/Commands/HueBridgeCommand.cs
+++ TeamFlash/Commands/HueBridgeCommand.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using ManyConsole;
 using Q42.HueApi;
 
@@ -8,15 +11,42 @@ namespace TeamFlash.Commands
     class HueBridgeCommand : ConsoleCommand
     {
         private string _timeoutString;
+        private bool _scan;
 
         public HueBridgeCommand()
         {
             IsCommand("huebridge", "Attempts to discover any Hue bridges on the network, and outputs their IP.");
             HasOption("t=|timeout=", "Maximum length of time in seconds to search for the bridge (default 15 seconds)", o => _timeoutString = o);
+            HasOption("s|scan", "Use IP address range scanning to find the bridge", o => _scan = o != null);
             SkipsCommandSummaryBeforeRunning();
         }
         public override int Run(string[] remainingArguments)
         {
+            if (_scan)
+            {
+                //TODO: Cant really assume /24 subnet...
+                var ip = String.Join(".",GetIpAddresss().Split('.').Take(3));
+                Parallel.ForEach(Enumerable.Range(0, 254), (lastByte, state) =>
+                    {
+                        try
+                        {
+                            var testHost = string.Format("{0}.{1}", ip, lastByte);
+                            var hostResource = String.Format("http://{0}/debug/clip.html", testHost);
+                            using (var client = new WebClient())
+                            {
+                                client.DownloadString(hostResource);
+                                Console.WriteLine("Found host at: {0}", testHost);
+                                state.Break();
+                            }
+                        }
+                        catch (WebException)
+                        {
+                        }
+                    });
+                Console.WriteLine("Failed to find any bridges via scan.  Ensure bridge is switched on and discoverable on your network segment.");
+                return 1;
+            }
+
             try
             {
                 var locator = new HttpBridgeLocator();
@@ -37,9 +67,19 @@ namespace TeamFlash.Commands
                 Console.WriteLine("Could not locate bridges due to error: {0} : {1}", e.Message, e.InnerException != null ? e.InnerException.Message : "");
                 throw;
             }
-
-            Console.WriteLine("Failed to find any bridges.  Ensure bridge is switched on and discoverable on your network segment.");
+            Console.WriteLine("Failed to find any bridges.  Ensure bridge Has internet access to register.");
             return 1;
+        }
+
+        private string GetIpAddresss()
+        {
+            var localIp = string.Empty;
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork))
+            {
+                localIp = ip.ToString();
+            }
+            return localIp;
         }
 
         private int GetTimeoutValue()
