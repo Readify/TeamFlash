85757b127d7deb31c2d37add9f31cf452a6dfd34
sheyenne <nick@enduroexplorer.com>
2013-11-25 18:37:40 +1100

    Add command line option to control intensity of Hue globes

--- TeamFlash/Commands/HueCommand.cs
+++ TeamFlash/Commands/HueCommand.cs
@@ -9,18 +9,20 @@ namespace TeamFlash.Commands
     {
         private string _ip;
         private string _lightsString;
+        private string _intensity;
 
         public HueCommand()
         {
             IsCommand("hue", "Start monitoring using a Philips Hue light");
             HasRequiredOption("ip=", "IP address to the Hue Light", o => _ip = o);
-            HasOption("lights=", "Comma delimeted list of light numbers", o =>_lightsString = o);
+            HasOption("lights=", "Comma delimeted list of light numbers", o => _lightsString = o);
+            HasOption("intensity=", "Intensity of bulb as a percentage", o => _intensity = o);
         }
 
         public override int Run(string[] remainingArguments)
         {
             var lights = string.IsNullOrEmpty(_lightsString) ? new List<string>{"1"} : _lightsString.Split(',').ToList();
-            BuildLight = new HueBuildLight(_ip, lights);
+            BuildLight = new HueBuildLight(_ip, lights, _intensity);
             return base.Run(remainingArguments);
         }
 
