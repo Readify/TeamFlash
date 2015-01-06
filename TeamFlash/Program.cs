165d46ccbc0d427793b642d51bec0aed6d2358bf
Dennis Doomen <dennis.doomen@avivasolutions.nl>
2015-01-06 07:44:39 +0100

    Changed the extension of the configuration file to better reflect its contents.

--- TeamFlash/Program.cs
+++ TeamFlash/Program.cs
@@ -7,9 +7,12 @@ using System.Xml.Serialization;
 
 namespace TeamFlash
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private const string ConfigFileName = @"config.xml";
+        private const string ConfigFolder = "TeamFlash";
+
+        private static void Main()
         {
             Logger.VerboseEnabled = PromptForVerboseMode();
 
@@ -18,7 +21,8 @@ namespace TeamFlash
             teamFlashConfig.ServerUrl = ReadConfig("TeamCity URL", teamFlashConfig.ServerUrl);
             teamFlashConfig.Username = ReadConfig("Username", teamFlashConfig.Username);
             var password = ReadConfig("Password", "");
-            teamFlashConfig.BuildTypeIds = ReadConfig("Comma separated build type ids (eg, \"bt64,bt12\"), or * for all", teamFlashConfig.BuildTypeIds);
+            teamFlashConfig.BuildTypeIds = ReadConfig(
+                "Comma separated build type ids (eg, \"bt64,bt12\"), or * for all", teamFlashConfig.BuildTypeIds);
 
             SaveConfig(teamFlashConfig);
 
@@ -26,7 +30,7 @@ namespace TeamFlash
 
             var buildTypeIds = teamFlashConfig.BuildTypeIds == "*"
                 ? new string[0]
-                : teamFlashConfig.BuildTypeIds.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                : teamFlashConfig.BuildTypeIds.Split(new[] {','}, StringSplitOptions.RemoveEmptyEntries).ToArray();
 
 #if __MonoCS__
             var buildLight = new Linux.BuildLight();
@@ -69,57 +73,63 @@ namespace TeamFlash
             buildLight.Off();
         }
 
-        static bool PromptForVerboseMode()
+        private static bool PromptForVerboseMode()
         {
             Console.Write("Would you like to start in VERBOSE mode? (y/n)");
             var selection = Console.ReadKey(true /*intercept*/).KeyChar;
             Console.WriteLine();
             var verbose = char.ToLower(selection) == 'y';
-            if(verbose) Console.WriteLine("VERBOSE mode is ON. Re-run TeamFlash in order to revert this.");
+            if (verbose) Console.WriteLine("VERBOSE mode is ON. Re-run TeamFlash in order to revert this.");
             Console.WriteLine();
             return verbose;
         }
 
-        static TeamFlashConfig LoadConfig()
+        private static TeamFlashConfig LoadConfig()
         {
             var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            var configFilePath = Path.Combine(appDataPath, @"TeamFlash\config.json");
+            var configFilePath = Path.Combine(appDataPath, ConfigFolder, ConfigFileName);
             try
             {
-                if (!File.Exists(configFilePath))return new TeamFlashConfig();
+                if (!File.Exists(configFilePath)) return new TeamFlashConfig();
 
                 Logger.WriteLine("Reading config values from: {0}", configFilePath);
 
-                var serializer = new XmlSerializer(typeof(TeamFlashConfig));
+                var serializer = new XmlSerializer(typeof (TeamFlashConfig));
                 using (var stream = File.OpenRead(configFilePath))
                 {
-                    return (TeamFlashConfig)serializer.Deserialize(stream);
+                    return (TeamFlashConfig) serializer.Deserialize(stream);
                 }
             }
             catch (Exception ex)
             {
-                Logger.WriteLine("The following exception occurred loading the config file \"{0}\":{2}Message: {1}{2}Feel free to quit and fix it or re-enter your server details...", configFilePath, ex.Message, Environment.NewLine);
+                Logger.WriteLine(
+                    "The following exception occurred loading the config file \"{0}\":{2}Message: {1}{2}Feel free to quit and fix it or re-enter your server details...",
+                    configFilePath, ex.Message, Environment.NewLine);
             }
+
             return new TeamFlashConfig();
         }
 
-        static void SaveConfig(TeamFlashConfig config)
+        private static void SaveConfig(TeamFlashConfig config)
         {
-            var appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-            var teamFlashPath = Path.Combine(appDataPath, @"TeamFlash\");
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string teamFlashPath = Path.Combine(appDataPath, ConfigFolder);
+
             if (!Directory.Exists(teamFlashPath))
+            {
                 Directory.CreateDirectory(teamFlashPath);
+            }
 
-            var configFilePath = Path.Combine(teamFlashPath, @"config.json");
+            string configFilePath = Path.Combine(teamFlashPath, ConfigFileName);
 
-            var serializer = new XmlSerializer(typeof(TeamFlashConfig));
+            XmlSerializer serializer = new XmlSerializer(typeof (TeamFlashConfig));
             using (var stream = File.OpenWrite(configFilePath))
             {
                 serializer.Serialize(stream, config);
             }
         }
 
-        static string ReadConfig(string name, string previousValue)
+        private static string ReadConfig(string name, string previousValue)
         {
             string input = null;
             while (string.IsNullOrEmpty(input))
@@ -140,19 +150,19 @@ namespace TeamFlash
             return input;
         }
 
-        static void Wait()
+        private static void Wait()
         {
             Logger.Verbose("Waiting for 30 seconds.");
             var delayCount = 0;
             while (delayCount < 30 &&
-                !Console.KeyAvailable)
+                   !Console.KeyAvailable)
             {
                 delayCount++;
                 Thread.Sleep(1000);
             }
         }
 
-        static BuildStatus RetrieveBuildStatus(
+        private static BuildStatus RetrieveBuildStatus(
             string serverUrl, string username, string password,
             IEnumerable<string> buildTypeIds)
         {
@@ -173,7 +183,8 @@ namespace TeamFlash
                     Logger.Verbose("Checking Project '{0}'.", project.Name);
                     if (!project.BuildTypesExists)
                     {
-                        Logger.Verbose("Bypassing Project '{0}' because it has no 'BuiltTypes' property defined.", project.Name);
+                        Logger.Verbose("Bypassing Project '{0}' because it has no 'BuiltTypes' property defined.",
+                            project.Name);
                         continue;
                     }
 
@@ -183,13 +194,18 @@ namespace TeamFlash
                         if (buildTypeIds.Any() &&
                             buildTypeIds.All(id => id != buildType.Id))
                         {
-                            Logger.Verbose("Bypassing Built Type '{0}\\{1}' because it does NOT match configured built-type list to monitor.", project.Name, buildType.Name);
+                            Logger.Verbose(
+                                "Bypassing Built Type '{0}\\{1}' because it does NOT match configured built-type list to monitor.",
+                                project.Name, buildType.Name);
                             continue;
                         }
 
-                        if (buildType.PausedExists && "true".Equals(buildType.Paused, StringComparison.CurrentCultureIgnoreCase))
+                        if (buildType.PausedExists &&
+                            "true".Equals(buildType.Paused, StringComparison.CurrentCultureIgnoreCase))
                         {
-                            Logger.Verbose("Bypassing Built Type '{0}\\{1}' because it has 'Paused' property set to 'true'.", project.Name, buildType.Name);
+                            Logger.Verbose(
+                                "Bypassing Built Type '{0}\\{1}' because it has 'Paused' property set to 'true'.",
+                                project.Name, buildType.Name);
                             continue;
                         }
 
@@ -197,18 +213,27 @@ namespace TeamFlash
                         var latestBuild = builds.First;
                         if (latestBuild == null)
                         {
-                            Logger.Verbose("Bypassing Built Type '{0}\\{1}' because no built history is available to it yet.", project.Name, buildType.Name);
+                            Logger.Verbose(
+                                "Bypassing Built Type '{0}\\{1}' because no built history is available to it yet.",
+                                project.Name, buildType.Name);
                             continue;
                         }
 
                         if ("success".Equals(latestBuild.Status, StringComparison.CurrentCultureIgnoreCase))
                         {
-                            dynamic runningBuild = new Query(serverUrl, username, password) { RestBasePath = string.Format("/httpAuth/app/rest/buildTypes/id:{0}/builds/running:any", buildType.Id) };
+                            dynamic runningBuild = new Query(serverUrl, username, password)
+                            {
+                                RestBasePath =
+                                    string.Format("/httpAuth/app/rest/buildTypes/id:{0}/builds/running:any",
+                                        buildType.Id)
+                            };
 
                             runningBuild.Load();
                             if ("success".Equals(runningBuild.Status, StringComparison.CurrentCultureIgnoreCase))
                             {
-                                Logger.Verbose("Bypassing Built Type '{0}\\{1}' because status of last build and all running builds are 'success'.", project.Name, buildType.Name);
+                                Logger.Verbose(
+                                    "Bypassing Built Type '{0}\\{1}' because status of last build and all running builds are 'success'.",
+                                    project.Name, buildType.Name);
                                 continue;
                             }
                         }
@@ -229,18 +254,19 @@ namespace TeamFlash
                                     "unstable".Equals(property.Value, StringComparison.CurrentCultureIgnoreCase))
                                 {
                                     isUnstableBuild = true;
-
                                 }
                             }
 
                             if (isUnstableBuild)
                             {
-                                Logger.Verbose("Bypassing Built Type '{0}\\{1}' because it is marked as 'unstable'.", project.Name, buildType.Name);
+                                Logger.Verbose("Bypassing Built Type '{0}\\{1}' because it is marked as 'unstable'.",
+                                    project.Name, buildType.Name);
                                 continue;
                             }
                         }
 
-                        Logger.Verbose("Now checking investigation status of Built Type '{0}\\{1}'.", project.Name, buildType.Name);
+                        Logger.Verbose("Now checking investigation status of Built Type '{0}\\{1}'.", project.Name,
+                            buildType.Name);
                         var buildId = buildType.Id;
                         dynamic investigationQuery = new Query(serverUrl, username, password);
                         investigationQuery.RestBasePath = @"/httpAuth/app/rest/buildTypes/id:" + buildId + @"/";
@@ -252,23 +278,26 @@ namespace TeamFlash
                             if ("taken".Equals(investigationState, StringComparison.CurrentCultureIgnoreCase) ||
                                 "fixed".Equals(investigationState, StringComparison.CurrentCultureIgnoreCase))
                             {
-                                Logger.Verbose("Investigation status of Built Type '{0}\\{1}' detected as either 'taken' or 'fixed'.", project.Name, buildType.Name);
+                                Logger.Verbose(
+                                    "Investigation status of Built Type '{0}\\{1}' detected as either 'taken' or 'fixed'.",
+                                    project.Name, buildType.Name);
                                 buildStatus = BuildStatus.Investigating;
                             }
                         }
 
                         if (buildStatus == BuildStatus.Failed)
                         {
-                            Logger.Verbose("Concluding status of Built Type '{0}\\{1}' as FAIL.", project.Name, buildType.Name);
+                            Logger.Verbose("Concluding status of Built Type '{0}\\{1}' as FAIL.", project.Name,
+                                buildType.Name);
                             return BuildStatus.Failed;
                         }
                     }
-
                 }
 
                 if (!couldFindProjects)
                 {
-                    Logger.Verbose("No Projects found! Please ensure if TeamCity URL is valid and also TeamCity setup and credentials are correct.");
+                    Logger.Verbose(
+                        "No Projects found! Please ensure if TeamCity URL is valid and also TeamCity setup and credentials are correct.");
                     return BuildStatus.Unavailable;
                 }
             }
